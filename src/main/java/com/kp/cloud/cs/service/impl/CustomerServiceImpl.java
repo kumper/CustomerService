79b897d2c782017f07a61c3149f65e2e3196b9a8
kumper <kumper@users.noreply.github.com>
2019-01-13 12:28:06 +0100

    show message for customer entity

--- src/main/java/com/kp/cloud/cs/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/kp/cloud/cs/service/impl/CustomerServiceImpl.java
@@ -14,14 +14,18 @@ import java.util.List;
  */
 @Service
 public class CustomerServiceImpl implements CustomerService {
+    
+    @Value("${customer-service.message:Failed to load property}")
+    private String message;
+    
     @Override
     public List<Customer> findAllCustomers() {
         return new ArrayList<>(
                 Arrays.asList(
-                        Customer.builder().customerNo("10").name("Great customer 1").build(),
-                        Customer.builder().customerNo("11").name("Green Hills").build(),
-                        Customer.builder().customerNo("12").name("Open Source Community").build(),
-                        Customer.builder().customerNo("13").name("ZaiX").build()
+                        Customer.builder().customerNo("10").name("Great customer 1").message(message).build(),
+                        Customer.builder().customerNo("11").name("Green Hills").message(message).build(),
+                        Customer.builder().customerNo("12").name("Open Source Community").message(message).build(),
+                        Customer.builder().customerNo("13").name("ZaiX").message(message).build()
                 )
         );
     }
